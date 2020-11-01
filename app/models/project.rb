218718dcd31120f4f1e74b1968bb721fa8c4a2bd
chamal <madusankechamal@gmail.com>
2020-11-01 16:29:21 +0000

    almost finished

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,38 @@
 class Project < ApplicationRecord
   has_many :tasks
   belongs_to :user
+
+
+def badge_color
+	case status
+when 'not-started'
+		'secondary'
+	when 'in-process'
+		'info'
+	when 'complete'
+		'success'
+	end	
+
 end
+
+def status
+	return 'not-started' if tasks.none?
+	if tasks.all? {|task| task.complete?}
+		'complete'
+		elsif tasks.any? {|task| task.in_progress?}
+			'in-progress'
+			
+
+end
+
+end
+
+def present_complete
+return 0 if tasks.none?
+complete_tasks = tasks.select {|task|task.complete?}.count
+((complete_tasks.to_f / tasks.count)*100).round 
+end
+
+
+end
+
--- app/models/task.rb
+++ app/models/task.rb
@@ -21,4 +21,17 @@ def badge_color
 
 end
 
+def complete?
+	status == 'complete'
+end
+
+def in_progress?
+	status == 'in-progress'
+end
+
+def not_started?
+	status == 'not-started'
+end
+
+
 end
